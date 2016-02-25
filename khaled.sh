a030a26fde940b71e25569963aa21009f5168e0f
nlaz <nikolazaris@utexas.edu>
2016-02-25 01:19:07 -0600

    :CAT: LION :CAT:

--- khaled.sh
+++ khaled.sh
@@ -10,4 +10,5 @@ NUM=$(wc -l < ${FILE})
 # generate random number in range 0-NUM
 let X="${RANDOM} % ${NUM} + 1"
 # extract X-th line
-sed -n ${X}p ${FILE}
+output=$(sed -n ${X}p ${FILE} | sed -e s/{1}/${1}/g | tr "[:lower:]" "[:upper:]")
+git commit -m "${output}"
