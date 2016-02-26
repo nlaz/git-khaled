481e4836a9816b807dffb962f4adb786d205e214
nlaz <nikolazaris@utexas.edu>
2016-02-26 17:36:18 -0600

    YOU SMART, YOU LOYAL, YOU CREATED COMMAND USAGE.

--- khaled.sh
+++ khaled.sh
@@ -6,10 +6,51 @@ FILE_NAME="khaled.txt"
 FILE="${FILE_PATH}/${FILE_NAME}"
 
 NUM=$(wc -l < ${FILE})
-MSG=$(echo ${1} | tr "[:lower:]" "[:upper:]")
+
+
+usage()
+{
+  
+  (echo "git-khaled - Record commits to your repository with DJ Khaled."
+   echo "USAGE: git khaled [options] <msg>"
+   echo "OPTIONS:"
+   echo "  --help                       prints this message"
+   echo "  -a, --all                    automatically stages files that have been modified and deleted"
+   echo "  -q, --quotes                 outputs all possible patterns") 1>&2
+}
+
+if [ $# -le 0 ]; then
+  echo "Oops! Invalid number of arguments \n"
+  usage
+  exit 0
+fi
+
+while [ $# -gt 0 ]
+do
+  case $1 in
+
+    --usage)
+      usage
+      exit 0
+      ;;
+
+    -a|--all);;
+
+    -q|--quotes);;
+
+    *)
+      if [ $# -gt 1 ]; then
+        usage
+        exit 1
+      fi
+      MSG=$(echo ${1} | tr "[:lower:]" "[:upper:]")
+      ;;
+  esac
+  shift 1
+done
 
 # generate random number in range 0-NUM
 let X="${RANDOM} % ${NUM} + 1"
-# extract X-th line
 OUTPUT=$(sed -n ${X}p ${FILE} | sed -e "s/{1}/${MSG}/g")
+
 git commit -m "${OUTPUT}"
